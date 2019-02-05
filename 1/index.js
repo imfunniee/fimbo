fdccbe53c20bb6b433f8a2f575a0736accf9c781
imfunny <36105478+imfunniee@users.noreply.github.com>
2019-02-05 15:54:45 +0530

    Add files via upload

--- 1/index.js
+++ 1/index.js
@@ -12,10 +12,14 @@ $(document).ready(function(){
     });
 });
 
+var width = $(window).width(); 
+
 window.onscroll = function(){
+if ((width >= 900)){
     if(document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
         $("#middle").css("background-size","150% auto");
     }else{
         $("#middle").css("background-size","100% auto");        
     }
+}
 };
