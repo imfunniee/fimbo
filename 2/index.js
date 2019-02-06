c02d5c763af38a3210a46cbe9fcf84ba9b3c1fb5
imfunny <36105478+imfunniee@users.noreply.github.com>
2019-02-06 11:29:16 +0530

    Add files via upload

--- 2/index.js
+++ 2/index.js
@@ -50,3 +50,18 @@ setTimeout(function(){
       $("#loading").css("display","none");
     },800);
 },1650);
+
+$(document).ready(function(){
+    $("a").on('click', function(event) {
+      if (this.hash !== "") {
+        event.preventDefault();
+        var hash = this.hash;
+        $('body,html').animate({
+        scrollTop: $(hash).offset().top
+        }, 1800, function(){
+        window.location.hash = hash;
+       });
+       } 
+      });
+  });
+  
