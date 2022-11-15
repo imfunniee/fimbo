43f324d66e3cf5adab751b6051e1ab846d434407
ZebraAlgebra <r10221026@ntu.eu.dtw>
2022-11-15 16:11:47 +0800

    QR-code update

--- index.js
+++ index.js
@@ -31,3 +31,10 @@ setTimeout(function(){
       $("#loading").css("display","none");
     },800);
 },1450);
+
+function onScanSuccess(decodedText, decodedResult) {
+  console.log(`Code scanned = ${decodedText}`, decodedResult);
+}
+var html5QrcodeScanner = new Html5QrcodeScanner(
+"qr-reader", { fps: 10, qrbox: 250 });
+html5QrcodeScanner.render(onScanSuccess);
