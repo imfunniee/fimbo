7584ce0616f94eefe057b2e77a567ad5e95ff7bc
ZebraAlgebra <r10221026@ntu.eu.dtw>
2022-11-22 14:30:11 +0800

    preview video plo player

---
+++ memories/unlocked.js
@@ -0,0 +1,11 @@
+setTimeout(function(){
+    $("#loading").addClass("animated fadeOut");
+    setTimeout(function(){
+      $("#loading").removeClass("animated fadeOut");
+      $("#loading").css("display","none");
+      $("#box").css("display","none");
+      $("#about").removeClass("animated fadeIn");
+      $("#contact").removeClass("animated fadeIn");
+      $("#work").removeClass("animated fadeIn");
+    },1000);
+},1500);
