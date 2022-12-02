dc84e17b340f22dc95b57b2f5734e5bcec852963
Yu-Ping Wu <yupingso@chromium.org>
2022-12-02 11:51:14 +0800

    Remove duplicate index.js

--- memories/index.js
+++ memories/index.js
@@ -56,19 +56,18 @@ setTimeout(function(){
 // Get the modal
 var modal = document.getElementById("myModal");
 
-// Get the image and insert it inside the modal - use its "alt" text as a caption
+// Get the image and insert it inside the modal
 var img = document.getElementById("hiddenimage");
 var modalImg = document.getElementById("img01");
 img.onclick = function(){
   modal.style.display = "block";
   modalImg.src = this.title;
-  captionText.innerHTML = "";
 }
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() { 
+span.onclick = function() {
   modal.style.display = "none";
 }
