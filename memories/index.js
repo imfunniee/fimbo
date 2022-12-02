c080026bfabed15c3ee33e3b52d0226197ea10c9
Yu-Ping Wu <yupingso@chromium.org>
2022-12-02 17:46:50 +0800

    Fix scrolling issue for modal image

--- memories/index.js
+++ memories/index.js
@@ -62,6 +62,7 @@ var modalImg = document.getElementById("img01");
 img.onclick = function(){
   modal.style.display = "block";
   modalImg.src = this.title;
+  $("body").css({"overflow-y":"hidden"});
 }
 
 // Get the <span> element that closes the modal
@@ -70,4 +71,5 @@ var span = document.getElementsByClassName("close")[0];
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
+  $("body").css({"overflow-y":"auto"});
 }
