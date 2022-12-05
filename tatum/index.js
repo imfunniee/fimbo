ef1a5a98257eb02b7483c817057741a55ef2c595
Yu-Ping Wu <yupingso@chromium.org>
2022-12-05 11:42:45 +0800

    Unify line endings to LF
    Add .gitattributes to enforce LF line endings for text files. Also
    remove trailing whitespaces.

--- tatum/index.js
+++ tatum/index.js
@@ -8,18 +8,18 @@ $(document).ready(function(){
       }, 1200, function(){
       window.location.hash = hash;
      });
-     } 
+     }
     });
 });
 
-var width = $(window).width(); 
+var width = $(window).width();
 
 window.onscroll = function(){
 if ((width >= 900)){
     if(document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
         $("#middle").css("background-size","150% auto");
     }else{
-        $("#middle").css("background-size","100% auto");        
+        $("#middle").css("background-size","100% auto");
     }
 }
 };
